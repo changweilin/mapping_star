f32932b8e44a611bb1d39c5b5d2412848f2b5ded
Willy Chang <x111281@gmail.com>
2026-05-21 23:08:11 +0800

    Deploy site with GitHub Pages Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "github-pages" ? "/mapping_star/" : "/",
   plugins: [react()],
   test: {
     environment: "node",
     globals: true
   }
-});
+}));
